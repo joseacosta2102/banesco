49eb39fc7280ab80547a0c8f1f9c66d03c57fb9d
Jose Acosta <jdap.dmc@gmail.com>
2024-04-18 10:15:27 -0400

    initial-commit

---
+++ enviroments.py
@@ -0,0 +1,7 @@
+clients = {
+    "thundernet barquisimeto": {
+        "SECRET": "TixW6CRKmAGm80aN193yKKuh0j8Y6Rif",
+        "API_KEY_BANESCO": "GJVVT2d7OVtWmk12ajpueneXxVv7vfe6",
+        "API_KEY": "jodas",
+    }
+}
